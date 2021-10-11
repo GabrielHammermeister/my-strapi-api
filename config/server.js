eba9c5da19698fc084df688111c395a3fc3111b5
Gabriel Hammer <gabriel.costa.ext@dasa.com.br>
2021-10-11 11:16:46 -0300

    Heroku configurations for deploy

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c4cb9ce62669854d9708a85ce8c21cf7'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '67f1eae0-154a-4526-ae64-6445c0d7876a'
+};
